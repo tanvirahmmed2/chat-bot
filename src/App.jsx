3a9c00e8cba0b0f9512a770efb46ac66541b81de
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-30 17:07:43 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosMenu } from "react-icons/io";
 
 const App = () => {
+    const [formData, setFormData]=useState({
+        language:'',
+        prompt:''
+    })
+
+
+    const handleChange=(e)=>{
+        const {name, value}= e.target
+        setFormData((prev)=>({...prev, [name]: value}))
+    }
+
+
     return (
         <div className='w-full overflow-x-hidden'>
             <div className='w-full relative'>
                 <div className='w-full fixed top-0 right-0'>
-                    <nav className='w-full flex flex-row items-center justify-between px-4 h-14 shadow-md'>
+                    <nav className='w-full flex flex-row items-center justify-between px-4 h-14 border-b-2 bg-white'>
                         <a href="/" className='font-serif text-xl font-semibold'>Chat-Bot</a>
                         <div>
                             <div>
@@ -16,11 +28,18 @@ const App = () => {
                         </div>
                     </nav>
                 </div>
-                <div>
-                    <div>
+                <div className='w-full flex flex-col'>
+                    <div className='w-full min-h-screen flex items-center justify-end flex-col p-4 gap-4'>
+                        <h1 className='w-full text-center font-semibold text-2xl'>Welcome to <span>chat bot</span></h1>
 
+                        <div className='w-full flex flex-col items-center justify-center md:w-3/4 gap-2'>
+                            <textarea name="prompt" id="prompt" onChange={handleChange} value={formData.prompt} className='w-full border-2 outline-none p-4 rounded-xl'></textarea>
+                            <button className='shadow-sm p-2 px-4 bg-gray-500  text-white rounded-lg'>Create content</button>
+                        </div>
                     </div>
-                    <footer>
+
+                    <footer className='w-full items-center flex justify-center py-4 border-t-2 '>
+                        <a href="https://disibin.com">Developed by Disibin</a>
 
                     </footer>
 
