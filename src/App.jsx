9e79fae549e3244cf4dfcffef16286a127f875f2
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-30 16:47:44 +0600

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full overflow-x-hidden'>
+        <div>
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default App
