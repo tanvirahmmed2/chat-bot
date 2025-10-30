b950532e7b0c0234ae26ddd6783aec788c0fd9a0
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-30 16:52:49 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
+import { IoIosMenu } from "react-icons/io";
 
 const App = () => {
-  return (
-    <div className='w-full overflow-x-hidden'>
-        <div>
-            <nav>
-                <a href="/">Chat-Bot</a>
-            </nav>
+    return (
+        <div className='w-full overflow-x-hidden'>
+            <div className='w-full relative'>
+                <div className='w-full fixed top-0 right-0'>
+                    <nav className='w-full flex flex-row items-center justify-between px-4 h-14 shadow-lg'>
+                        <a href="/" className='italic text-xl font-semibold'>Chat-Bot</a>
+                        <div>
+                            <div>
+
+                            </div>
+                            <button className='text-2xl '><IoIosMenu /></button>
+                        </div>
+                    </nav>
+                </div>
+                <div>
+                    <div>
+
+                    </div>
+                    <footer>
+                        
+                    </footer>
+
+                </div>
+            </div>
+
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default App
