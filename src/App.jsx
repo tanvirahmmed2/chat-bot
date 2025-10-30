5a682f06de45c813abd6977e77ba46c19f22cb7d
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-10-30 16:48:49 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ const App = () => {
   return (
     <div className='w-full overflow-x-hidden'>
         <div>
-            
+            <nav>
+                <a href="/">Chat-Bot</a>
+            </nav>
         </div>
       
     </div>
